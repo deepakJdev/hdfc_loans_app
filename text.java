68df66936074b1b540beda75379df6ffc6341a3a
deepakJdev <deepak.dev8950@gmail.com>
2026-05-09 10:56:26 +0530

    Update text.java

--- text.java
+++ text.java
@@ -3,6 +3,9 @@ class myclass{
      System.out.println("hello this is text file");
      System.out.println("hello this is text file");
      System.out.println("hello this is text file");
-     
+
+public myclass(){
+    System.out.println("defualt constructor .....");
+}
 
 }
